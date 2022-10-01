6423247b0a8d8905dd13c884a7716ab306bc1304
Che Ndumbi <che.ndumbi@gmail.com>
2022-10-01 17:02:08 -0400

    fixed profile and item delete link views for unpermitted users

--- crystalthrift/views.py
+++ crystalthrift/views.py
@@ -5,7 +5,11 @@ from thrift.models import Item
 
 def home_view(request):
     # list all items in the data base by the time they were created
-    items = Item.objects.order_by('-created').all()
+    items = []
+    if request.user.is_authenticated:
+        items = Item.objects.exclude(user=request.user).order_by('-created')
+    else:
+        items = Item.objects.order_by('-created')
     context = {
         "items": items,
         "types": get_item_types_list,
